80d7e5cfcc1b5afe8cc47ed5aa9a928858c12c45
Xin Deng <xind1470@gmail.com>
2024-02-20 14:19:18 -0800

    added urls to project

--- snacks_api_project/urls.py
+++ snacks_api_project/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for snacks_api_project project.
+URL configuration for things_api_project project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/5.0/topics/http/urls/
@@ -15,8 +15,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path("api/v1/snacks/", include("snacks.urls")),
+    path("api-auth/", include("rest_framework.urls")),
+
 ]
