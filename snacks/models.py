bdb676fb607d2e8471555758b9e947bd0741a28d
Xin Deng <xind1470@gmail.com>
2024-02-20 14:16:01 -0800

    added models to app

--- snacks/models.py
+++ snacks/models.py
@@ -1,3 +1,13 @@
+from django.contrib.auth import get_user_model
 from django.db import models
 
-# Create your models here.
+
+class Snack(models.Model):
+    purchaser = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
+    name = models.CharField(max_length=64)
+    description = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name
