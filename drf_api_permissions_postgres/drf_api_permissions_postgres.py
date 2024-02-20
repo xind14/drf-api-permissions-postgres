994d163d9396a4c957256f3e4dcc6af8bdc9c7b2
Xin Deng <xind1470@gmail.com>
2024-02-20 10:06:46 -0800

    "First Commit"

---
+++ drf_api_permissions_postgres/drf_api_permissions_postgres.py
@@ -0,0 +1,14 @@
+
+# Import stuff
+
+# From stuff import things
+
+    
+
+def drf_api_permissions_postgres(demo_fixture=""):
+
+    if demo_fixture:
+        return demo_fixture
+ 
+    return "Starter test"
+
